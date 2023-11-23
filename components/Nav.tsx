f5d8b49f5342d1c4e6cf30a78138b4492c798141
Devin MacGillivray <devhmac@gmail.com>
2023-11-22 19:46:03 -0700

    nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,3 +1,4 @@
+import { Search, Star, User } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -5,12 +6,17 @@ import React from "react";
 const Nav = () => {
   return (
     <header>
-      <nav className="flex justify-between items-center px-6 md:px-20 py-4">
+      <nav className="nav">
         <Link href="/" className="flex items-center gap-1">
           {/* <Image /> */}
           <div>icon</div>
           <p className="text-xl">Minr</p>
         </Link>
+        <div className="flex items-center justify-between gap-4">
+          <Search className="w-5 h-5" />
+          <Star className="w-5 h-5" />
+          <User className="w-5 h-5" />
+        </div>
       </nav>
     </header>
   );
