326e1ac02e4d179c352b2b9199e8c94750465382
Devin MacGillivray <devhmac@gmail.com>
2023-12-03 14:11:38 -0700

    building incoming product object

--- lib/actions/actions.ts
+++ lib/actions/actions.ts
@@ -1,7 +1,10 @@
 "use server";
 
+import { PriceHistoryItem } from "@/types";
+import Product from "../models/product.model";
 import { dbConnect } from "../mongoose";
 import { scrapeUrl } from "../scraper";
+import { getHighestPrice, getLowestPrice } from "../utils/extractFunctions";
 
 export async function scrapeProduct(productUrl: string) {
   if (!productUrl) return;
@@ -12,8 +15,25 @@ export async function scrapeProduct(productUrl: string) {
     console.log(scrapedProduct);
 
     dbConnect();
+    let product = scrapedProduct;
+    const existingProduct = await Product.findOne({ url: scrapedProduct.url });
 
-    const existingProduct = await Product;
+    // if product already exists we need to update it
+    if (existingProduct) {
+      //update the price history on the prod with the new current price
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice },
+      ];
+
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(),
+      };
+    }
   } catch (error) {
     throw new Error(`Failed to create/update product: ${error}`);
   }
