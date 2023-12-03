dc248a47d17468d2571f19e5e5a5ccf2b8c1de4e
Devin MacGillivray <devhmac@gmail.com>
2023-12-03 13:11:28 -0700

    setting up mongoose connection

---
+++ lib/mongoose.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const dbConnect = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URI) return console.log("no MONGODB_URI defined");
+
+  if (isConnected) {
+    return console.log("still connected to mongodb ");
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    isConnected = true;
+    console.log("connected to mongo");
+  } catch (error) {
+    console.log(error);
+  }
+};
