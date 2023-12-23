be71ddcc7853fb6d66c6a72b2217532e3735730b
devhmac <devhmac@gmail.com>
2023-12-23 14:42:07 -0700

    getting product from urlid

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,10 +1,29 @@
+import { getProductById } from "@/lib/actions/actions";
+import { Product } from "@/types";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 import React from "react";
+
 type Props = {
   params: { id: string };
 };
 
-const page = ({ params: { id } }: Props) => {
-  return <div>{id}</div>;
+const ProductDetails = async ({ params: { id } }: Props) => {
+  const product: Product = await getProductById(id);
+
+  if (!product) redirect("/");
+
+  return (
+    <div>
+      <div>{product.title}</div>
+      <Image
+        src={product.image}
+        alt="Picture of Product"
+        height={400}
+        width={400}
+      />
+    </div>
+  );
 };
 
-export default page;
+export default ProductDetails;
