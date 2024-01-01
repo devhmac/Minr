8873b6f6d4bd7af5a72d262191700f6bf90c7627
devhmac <devhmac@gmail.com>
2024-01-01 13:13:42 -0700

    more styling

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -19,15 +19,19 @@ const ProductDetails = async ({ params: { id } }: Props) => {
   return (
     <section className="px-6 md:px-20 py-24">
       <h3 className="text-secondary text-semibold">{product.title}</h3>
+
       <div className="flex flex-col lg:flex-row justify-center gap-6">
-        <div className="flex flex-col w-auto max-w-[500px] mx-auto lg:max-w-3xl lg:max-h-[400px] bg-red-300">
-          <Image
-            src={product.image}
-            alt="Picture of Product"
-            height={400}
-            width={400}
-            className="Object-conatin rounded-md bg-white"
-          />
+        <div className="relative sm:px-auto py-5 sm:pt-5 pb-5  w-full border-2 border-lowestEmph rounded-[30px] mx-auto sm:max-w-xl  lg:max-w-md lg:h-[400px]">
+          {/* <div className="flex flex-col w-auto max-w-[500px] mx-auto lg:max-w-3xl lg:max-h-[400px] bg-red-300"> */}
+          <div>
+            <Image
+              src={product.image}
+              alt="Picture of Product"
+              height={200}
+              width={200}
+              className="hh-full w-full bg-transparent max-h-[300px] max-w-[300px] object-contain"
+            />
+          </div>
           <div className="flex flex-row justify-between">
             <p>stat 1</p>
             <p>stat 2</p>
@@ -36,13 +40,18 @@ const ProductDetails = async ({ params: { id } }: Props) => {
         </div>
         {/* <p> bro:{JSON.stringify(product)}</p> */}
 
-        <div className="relative sm:px-10 py-5 sm:pt-20 pb-5  w-full border-2 border-lowestEmph rounded-[30px] mx-auto max-w-[250px] h-[150px] sm:max-w-xl sm:h-[200px] lg:max-w-3xl lg:h-[400px]">
+        <div className="relative sm:px-10 py-5 sm:pt-20 pb-5  w-full border-2 border-lowestEmph rounded-[30px] mx-auto h-[150px] sm:max-w-xl sm:h-[200px] lg:max-w-3xl lg:h-[400px]">
           <ProductLineChart data={data} />
         </div>
       </div>
-      <Link className="searchbar-btn " href={product.url}>
-        To Amazon Product
-      </Link>
+      <div className="flex justify-end">
+        <Link className="mx-2" href={product.url}>
+          Amazon
+        </Link>
+        <Link className="" href="/">
+          Back
+        </Link>
+      </div>
     </section>
   );
 };
