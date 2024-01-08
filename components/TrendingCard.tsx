c7a1b833c7aa53982c4dd35cbeb8207a73a2a2a0
Devin MacGillivray <devhmac@gmail.com>
2024-01-08 16:31:50 -0700

    testing line chart in trending card

--- components/TrendingCard.tsx
+++ components/TrendingCard.tsx
@@ -2,6 +2,8 @@ import { Product } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import ProductLineChart from "./ProductLineChart";
+import data from "@/lib/static/exampleData";
 
 type Props = {
   product: Product;
@@ -14,6 +16,7 @@ const TrendingCard = ({ product }: Props) => {
       title={product.title}
       className="product-card border-2 border-bentoBackground bg-lower rounded-md items-center p-2 bg-bentoBackground hover:bg-primary hover:shadow hover:shadow-lowestEmph hover:bg-opacity-70"
     >
+      {/* <div className="flex flex-row w-xl"> */}
       <div className="product-card_img-container bg-white h-full w-full ">
         {product.discountRate > 0 ? (
           <p className="absolute bg-primary text-secondary justify-center py-1 px-1.5 rounded-md left-1/2 -translate-x-1/2 border-gray-200 border">
@@ -28,7 +31,9 @@ const TrendingCard = ({ product }: Props) => {
           className="h-full w-full bg-transparent max-h-[200px] max-w-[250px] object-contain rounded-md "
         />
       </div>
-
+      <div>
+        <ProductLineChart data={data} />
+      </div>
       <div className="flex flex-col gap-2">
         <h3 className="text-secondary text-l leading-5 truncate font-semibold w-[200px]">
           {product.title}
