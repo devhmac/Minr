df23e9603f9af69bd2b1d29dce8c36bc0656649d
Devin MacGillivray <devhmac@gmail.com>
2024-01-08 16:38:16 -0700

    change of flex 1 makes chart appear

--- components/TrendingCard.tsx
+++ components/TrendingCard.tsx
@@ -16,22 +16,22 @@ const TrendingCard = ({ product }: Props) => {
       title={product.title}
       className="product-card border-2 border-bentoBackground bg-lower rounded-md items-center p-2 bg-bentoBackground hover:bg-primary hover:shadow hover:shadow-lowestEmph hover:bg-opacity-70"
     >
-      {/* <div className="flex flex-row w-xl"> */}
-      <div className="product-card_img-container bg-white h-full w-full ">
-        {product.discountRate > 0 ? (
-          <p className="absolute bg-primary text-secondary justify-center py-1 px-1.5 rounded-md left-1/2 -translate-x-1/2 border-gray-200 border">
-            {product.discountRate}% Off
-          </p>
-        ) : null}
-        <Image
-          src={product.image}
-          alt="product image"
-          width="200"
-          height="200"
-          className="h-full w-full bg-transparent max-h-[200px] max-w-[250px] object-contain rounded-md "
-        />
-      </div>
-      <div>
+      <div className="flex flex-row">
+        <div className="product-card_img-container bg-white h-full w-full ">
+          {product.discountRate > 0 ? (
+            <p className="absolute bg-primary text-secondary justify-center py-1 px-1.5 rounded-md left-1/2 -translate-x-1/2 border-gray-200 border">
+              {product.discountRate}% Off
+            </p>
+          ) : null}
+          <Image
+            src={product.image}
+            alt="product image"
+            width="200"
+            height="200"
+            className="h-full w-full bg-transparent max-h-[200px] max-w-[250px] object-contain rounded-md "
+          />
+        </div>
+
         <ProductLineChart data={data} />
       </div>
       <div className="flex flex-col gap-2">
