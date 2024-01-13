40d57d4d4298a0ad428cc02bba28085cc1b0ae0b
Devin MacGillivray <devhmac@gmail.com>
2024-01-12 17:31:21 -0700

    to product link

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -31,11 +31,11 @@ const ProductDetails = async ({ params: { id } }: Props) => {
   return (
     <section className="px-6 md:px-20 py-24">
       <h3 className="text-secondary text-semibold">{product.title}</h3>
-      <div className="flex flex-col lg:flex-row justify-center items-center gap-6">
+      <div className="flex flex-col lg:flex-row justify-center items-center gap-6 mt-6">
         <div className="justify-center items-center sm:px-auto sm:pt-5 pb-5  w-1/3 sm:max-w-xl lg:h-[400px]">
           {/* <div className="flex flex-col w-auto max-w-[500px] mx-auto lg:max-w-3xl lg:max-h-[400px] bg-red-300 items-center bg-white rounded-md mx-5"> */}
           {/* <div className="flex  items-center justify-center"> */}
-          <div className="justify-center items-center product-card_img-container bg-white h-full w-full max-h-[400px] max-w-auto overflow-hidden ">
+          <div className="justify-center items-center product-card_img-container bg-white h-full w-full max-h-[400px] max-w-auto overflow-hidden mb-2">
             <Image
               src={product.image}
               alt="Picture of Product"
@@ -44,6 +44,13 @@ const ProductDetails = async ({ params: { id } }: Props) => {
               className="h-full w-auto bg-transparent max-h-[400px] max-w-[300px] object-contain rounded-md"
             />
           </div>
+          <Link
+            className=" text-mediumEmph cursor-pointer"
+            href={product.url}
+            title="To product url"
+          >
+            <button>Visit Product Page</button>
+          </Link>
           {/* </div> */}
         </div>
 
