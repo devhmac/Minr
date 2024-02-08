7f7a55cb02e82e49690664a3d2087932b99a1717
Devin MacGillivray <devhmac@gmail.com>
2024-02-07 20:16:27 -0700

    adding date to make typescript happy

--- lib/static/exampleData.ts
+++ lib/static/exampleData.ts
@@ -2,30 +2,37 @@ const data = [
   {
     average: 400,
     price: 240,
+    date: null,
   },
   {
     average: 300,
     price: 139,
+    date: null,
   },
   {
     average: 200,
     price: 980,
+    date: null,
   },
   {
     average: 278,
     price: 390,
+    date: null,
   },
   {
     average: 189,
     price: 480,
+    date: null,
   },
   {
     average: 239,
     price: 380,
+    date: null,
   },
   {
     average: 349,
     price: 430,
+    date: null,
   },
 ];
 
