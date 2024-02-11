291e596c5bc732a4cfb6dfefa69661c0348efe94
devhmac <devhmac@gmail.com>
2024-02-11 11:12:45 -0700

    cron route started

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,15 +1,18 @@
 import { getAllProducts } from "@/lib/actions/actions";
 import { dbConnect } from "@/lib/mongoose";
 import { scrapeUrl } from "@/lib/scraper";
+import { Product } from "@/types";
 
 export async function GET() {
   const products = await getAllProducts();
 
   if (!products || products.length === 0) throw new Error("No Products found");
 
-  const cronUpdatedProducts = await Promise.all(
-    products.map(async (product) => {
-      scrapeUrl(product.url);
+  const allUpdatedProducts = await Promise.all(
+    products.map(async (product: Product) => {
+      const newScrape = await scrapeUrl(product.url);
+
+      if (!newScrape) throw new Error("Automated Recurring Scrape Failed");
     })
   );
 }
