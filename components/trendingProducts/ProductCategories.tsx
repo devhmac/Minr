6a9b08f0bab9542c0df51a9258e9346156d3aab8
devhmac <devhmac@gmail.com>
2024-02-17 14:39:19 -0700

    commented out code cleanup

--- components/trendingProducts/ProductCategories.tsx
+++ components/trendingProducts/ProductCategories.tsx
@@ -22,26 +22,6 @@ const ProductCategories = ({ products: data, categories }: Props) => {
 
   let topCategories = currentCategories.splice(0, 6);
 
-  // type CategoryCounts = { [key: string]: number };
-  // const categoryCounts: CategoryCounts = products.reduce(
-  //   (acc: CategoryCounts, product) => {
-  //     acc[product.category] = (acc[product.category] || 0) + 1;
-  //     return acc;
-  //   },
-  //   {}
-  // );
-  // const topCategories = useMemo(() => {
-  //   return currentCategories.sort((a, b) => b.count - a.count).splice(0, 6);
-  // }, currentCategories);
-
-  // let topCategories = Object.keys(categoryCounts)
-  //   .map((cat) => ({
-  //     category: cat,
-  //     count: categoryCounts[cat],
-  //   }))
-  //   .sort((a, b) => b.count - a.count)
-  //   .splice(0, 6);
-
   topCategories = [{ category: "All", count: 1 }, ...topCategories];
 
   return (
