a698ee8fffe0123f5b1b61a9c32b45b01fcdbc25
devhmac <devhmac@gmail.com>
2024-02-23 07:34:50 -0700

    nice bookmark selected state

--- components/trendingProducts/Bookmark.tsx
+++ components/trendingProducts/Bookmark.tsx
@@ -1,18 +1,27 @@
+"use client";
 import { BookmarkCheck, BookmarkIcon } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 
 const Bookmark = ({ productId }: { productId: string }) => {
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
   const clickhandler = (e: any) => {
     e.preventDefault();
     e.stopPropagation();
-    console.log("bookmarked");
+    setIsBookmarked((prev) => {
+      // if !prev is true, send bookmark increment to database, if false remove bookmark increment
+      return !prev;
+    });
   };
 
   return (
     <div title="Bookmark Product" onClick={clickhandler}>
-      {/* Bookmark */}
-      {/* <BookmarkCheck /> */}
-      <BookmarkIcon className="text-zinc-600 bg-white hover:bg-primary rounded-md h-7 w-7 p-1 hover:text-mediumEmph " />
+      {isBookmarked ? (
+        <BookmarkCheck className="text-zinc-700 bg-white fill-primary  rounded-md h-7 w-7 p-1   " />
+      ) : (
+        // <BookmarkIcon className="text-zinc-600 bg-white hover:bg-primary rounded-md h-7 w-7 p-1 hover:fill-white   " />
+        <BookmarkIcon className="text-zinc-600 bg-white  rounded-md h-7 w-7 p-1 hover:fill-zinc-400   " />
+      )}
     </div>
   );
 };
