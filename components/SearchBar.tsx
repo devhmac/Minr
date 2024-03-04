3090bb50b90a9c0788a5afda8a0beb789f7570af
Devin MacGillivray <devhmac@gmail.com>
2024-03-03 20:31:25 -0700

    messing around with the button too much

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -137,14 +137,26 @@ const SearchBar = () => {
           />
         )}
 
-        <button
+        <div className="relative inline-flex group">
+          <div className="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r  from-primary via-[#ff44ecb6] to-[#44BCFF]  rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
+
+          <button
+            title="Get quote now"
+            className="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-[rgba(18, 18, 18, 1)] font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 border-lowEmph border-2"
+            role="button"
+            disabled={true}
+          >
+            Scrape
+          </button>
+        </div>
+
+        {/* <button
           type="submit"
           className="searchbar-btn "
           disabled={searchPrompt === "" || isLinkValid !== true}
         >
           {isLoading ? <Loader2 className="h-4 w-4 animate-spin" /> : "Scrape"}
-          {/* <ProgressBar progress={uploadProgress} /> */}
-        </button>
+        </button> */}
       </form>
 
       {searchPrompt !== "" && isLinkValid === false && (
