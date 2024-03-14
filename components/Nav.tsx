fc6c92fcec5de0e0127ddbbaa48db7a64e9430ca
devhmac <devhmac@gmail.com>
2024-03-14 07:09:39 -0600

    adding some hover on bookmarks

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,4 @@
-import { Search, Star, User } from "lucide-react";
+import { BookmarkIcon, Search, Star, User } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -6,21 +6,20 @@ import React from "react";
 const Nav = () => {
   return (
     <header>
-      <nav className="nav">
+      <nav className="nav ">
         <Link href="/" className="flex items-center gap-1">
           <Image src="/minr_logo.png" height={25} width={25} alt="Minr Logo" />
           <p className="text-xl text-primary font-semibold">Minr</p>
         </Link>
-        <div className="flex items-center justify-between gap-4 ">
-          <Search className="w-5 h-5 text-secondary" />
+        <div className="flex items-center justify-between gap-4 h-full  hover:bg-lowEmph hover:h-full ">
           <Link
             href={"/bookmarks"}
             title="My Bookmarks"
-            className="hover:bg-lowEmph "
+            className=" flex flex-row gap-2 "
           >
-            <Star className="w-5 h-5 text-secondary" />
+            <BookmarkIcon className="text-secondary  rounded-md w-5 h-5   " />
+            Bookmarks
           </Link>
-          <User className="w-5 h-5 text-secondary" />
         </div>
       </nav>
     </header>
